8419c7ab731c90e6af28e9406e8f7a64511344a8
mattgwin29 <mattgwin@udel.edu>
2023-09-21 22:28:21 -0400

    Add functioning navbar generated

---
+++ javascript/site.js
@@ -0,0 +1,16 @@
+function loadNavBar() {
+
+    document.getElementsByClassName("navbar")[0].innerHTML=
+        `        
+        <ul>
+        <li><a href="./">Home</a></li>
+        <li><a href="./aboutMe.html">About Me</a></li>
+        <li><a href="./contact.html">Contact</a></li>
+        <li><a href="https://mattm401.github.io/">CISC474 Website</a></li>
+        <li><a href="https://rgud0.github.io/">Previous Student</a></li>
+        <li><a href="https://hkaur02.github.io/">Next Student</a></li>
+        </ul>
+        `;
+}
+
+loadNavBar();
