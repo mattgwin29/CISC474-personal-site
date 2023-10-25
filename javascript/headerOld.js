dc99332fcca040478cc6c7767f605aa538562b04
mattgwin29 <mattgwin@udel.edu>
2023-10-25 15:37:14 -0400

    Add download to resume website

--- javascript/headerOld.js
+++ javascript/headerOld.js
@@ -6,6 +6,7 @@ function loadNavBar() {
         <li><a href="./">Home</a></li>
 
         <li><a href="./aboutMe.html">About Me</a></li>
+        <li><a href="./resume.html">Resume</a></li>
         <li><a href="./contact.html">Contact</a></li>
         <li><div class="dropdown">
             <button class="dropbtn">Class Links</button>
