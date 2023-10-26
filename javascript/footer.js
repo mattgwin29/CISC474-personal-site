2646d591f46c70a41a9d59a2dd2857d00390489f
mattgwin29 <mattgwin@udel.edu>
2023-10-26 09:00:02 -0400

    Fix padding issues on footer

--- javascript/footer.js
+++ javascript/footer.js
@@ -1,16 +1,6 @@
 function loadFooter(){
     document.getElementsByTagName("footer")[0].innerHTML = ` 
-    <ul class="social-icon">
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
-          <ion-icon name="logo-github"></ion-icon>
-        </a></li>
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
-          <ion-icon name="logo-linkedin"></ion-icon>
-        </a></li>
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
-          <ion-icon name="logo-email"></ion-icon>
-        </a></li>
-    </ul>
+
     <ul class="menu">
       <img src="./assets/github.png" href="https://github.com/mattgwin29">
       <li class="menu__item"><a class="menu__link" href="https://github.com/mattgwin29">GitHub</a></li>
